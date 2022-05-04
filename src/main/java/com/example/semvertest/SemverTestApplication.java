b42f9bff3a36563c51099f4433fa2af895ef17f1
Dmytro Shvets <dmytro.shvets@ge.com>
2022-05-04 15:37:55 +0300

    Initial commit

---
+++ src/main/java/com/example/semvertest/SemverTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.semvertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemverTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemverTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/semvertest/SemverTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.semvertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemverTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
